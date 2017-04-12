f5f19f89443eb7d2dce69b7e3554a5a163ed8f00
Jason Walton <jwalton@benbria.ca>
2017-04-12 15:49:43 -0400

    Add prefer-promise-reject-errors, convert config to strings instead of numbers.

--- index.js
+++ index.js
@@ -33,10 +33,11 @@ module.exports = {
     },
 
     "rules": {
-        "no-console": 0, // disallow use of console (off by default in the node environment)
-        "valid-jsdoc": 2, // Ensure JSDoc comments are valid (off by default)
-        "no-unsafe-negation": 2, // disallow negation of the left operand of an in expression
-        "comma-dangle": 2, // disallow or enforce trailing commas
+        "no-console": "off", // disallow use of console (off by default in the node environment)
+        "no-extra-parens": "off",
+        "valid-jsdoc": "error", // Ensure JSDoc comments are valid (off by default)
+        "no-unsafe-negation": "error", // disallow negation of the left operand of an in expression
+        "comma-dangle": "error", // disallow or enforce trailing commas
 
         //
         // Best Practices
@@ -44,42 +45,42 @@ module.exports = {
         // These are rules designed to prevent you from making mistakes.
         // They either prescribe a better way of doing something or help you avoid footguns.
         //
-        "consistent-return": 2, // require return statements to either always or never specify values
-        "curly": 2, // specify curly brace conventions for all control statements
-        "default-case": 2, // require default case in switch statements (off by default)
-        "eqeqeq": 2, // require the use of === and !==
-        "guard-for-in": 2, // make sure for-in loops have an if statement (off by default)
-        "no-alert": 2, // disallow the use of alert, confirm, and prompt
-        "no-caller": 2, // disallow use of arguments.caller or arguments.callee
-        "no-div-regex": 2, // disallow division operators explicitly at beginning of regular expression (off by default)
-        "no-eq-null": 2, // disallow comparisons to null without a type-checking operator (off by default)
-        "no-eval": 2, // disallow use of eval()
-        "no-extend-native": 2, // disallow adding to native types
-        "no-extra-bind": 2, // disallow unnecessary function binding
-        "no-floating-decimal": 2, // disallow the use of leading or trailing decimal points in numeric literals (off by default)
-        "no-implied-eval": 2, // disallow use of eval()-like methods
-        "no-iterator": 2, // disallow usage of __iterator__ property
-        "no-labels": 2, // disallow use of labeled statements
-        "no-lone-blocks": 2, // disallow unnecessary nested blocks
-        "no-multi-str": 2, // disallow use of multiline strings
-        "no-global-assign": 2, // disallow reassignments of native objects
-        "no-new": 2, // disallow use of new operator when not part of the assignment or comparison
-        "no-new-func": 2, // disallow use of new operator for Function object
-        "no-new-wrappers": 2, // disallows creating new instances of String,Number, and Boolean
-        "no-octal-escape": 2, // disallow use of octal escape sequences in string literals, such as var foo = "Copyright \251";
-        "no-proto": 2, // disallow usage of __proto__ property
-        "no-return-assign": 2, // disallow use of assignment in return statement
-        "no-script-url": 2, // disallow use of javascript: urls.
-        "no-self-compare": 2, // disallow comparisons where both sides are exactly the same (off by default)
-        "no-sequences": 2, // disallow use of comma operator
-        "no-throw-literal": 2, // restrict what can be thrown as an exception (off by default)
-        // disallow usage of configurable warning terms in comments": 2, // e.g. TODO or FIXME (off by default)
-        "no-warning-comments": [1, {"terms": ["todo", "fixme"], "location": "start"}],
-        "no-with": 2, // disallow use of the with statement
-        "radix": 2, // require use of the second argument for parseInt() (off by default)
-        "vars-on-top": 2, // requires to declare all vars on top of their containing scope (off by default)
-        "wrap-iife": 2, // require immediate function invocation to be wrapped in parentheses (off by default)
-        "yoda": 0, // require or disallow Yoda conditions
+        "consistent-return": "error", // require return statements to either always or never specify values
+        "curly": "error", // specify curly brace conventions for all control statements
+        "default-case": "error", // require default case in switch statements (off by default)
+        "eqeqeq": "error", // require the use of === and !==
+        "guard-for-in": "error", // make sure for-in loops have an if statement (off by default)
+        "no-alert": "error", // disallow the use of alert, confirm, and prompt
+        "no-caller": "error", // disallow use of arguments.caller or arguments.callee
+        "no-div-regex": "error", // disallow division operators explicitly at beginning of regular expression (off by default)
+        "no-eq-null": "error", // disallow comparisons to null without a type-checking operator (off by default)
+        "no-eval": "error", // disallow use of eval()
+        "no-extend-native": "error", // disallow adding to native types
+        "no-extra-bind": "error", // disallow unnecessary function binding
+        "no-floating-decimal": "error", // disallow the use of leading or trailing decimal points in numeric literals (off by default)
+        "no-implied-eval": "error", // disallow use of eval()-like methods
+        "no-iterator": "error", // disallow usage of __iterator__ property
+        "no-labels": "error", // disallow use of labeled statements
+        "no-lone-blocks": "error", // disallow unnecessary nested blocks
+        "no-multi-str": "error", // disallow use of multiline strings
+        "no-global-assign": "error", // disallow reassignments of native objects
+        "no-new": "error", // disallow use of new operator when not part of the assignment or comparison
+        "no-new-func": "error", // disallow use of new operator for Function object
+        "no-new-wrappers": "error", // disallows creating new instances of String,Number, and Boolean
+        "no-octal-escape": "error", // disallow use of octal escape sequences in string literals, such as var foo = "Copyright \251";
+        "no-proto": "error", // disallow usage of __proto__ property
+        "no-return-assign": "error", // disallow use of assignment in return statement
+        "no-script-url": "error", // disallow use of javascript: urls.
+        "no-self-compare": "error", // disallow comparisons where both sides are exactly the same (off by default)
+        "no-sequences": "error", // disallow use of comma operator
+        "no-throw-literal": "error", // restrict what can be thrown as an exception (off by default)
+        "no-warning-comments": ["warn", {"terms": ["todo", "fixme"], "location": "start"}],
+        "no-with": "error", // disallow use of the with statement
+        "prefer-promise-reject-errors": "error", // reject() should always be called with an error.
+        "radix": "error", // require use of the second argument for parseInt() (off by default)
+        "vars-on-top": "error", // requires to declare all vars on top of their containing scope (off by default)
+        "wrap-iife": "error", // require immediate function invocation to be wrapped in parentheses (off by default)
+        "yoda": "off", // require or disallow Yoda conditions
         "semi": ["error", "always"],
 
         //
@@ -87,11 +88,11 @@ module.exports = {
         //
         // These rules have to do with variable declarations.
         //
-        "no-catch-shadow": 2, // disallow the catch clause parameter name being the same as a variable in the outer scope (off by default in the node environment)
-        "no-label-var": 2, // disallow labels that share a name with a variable
-        "no-shadow-restricted-names": 2, // disallow shadowing of names such as arguments
-        "no-undef-init": 2, // disallow use of undefined when initializing variables
-        "no-unused-vars": 2, // disallow declaration of variables that are not used in the code
+        "no-catch-shadow": "error", // disallow the catch clause parameter name being the same as a variable in the outer scope (off by default in the node environment)
+        "no-label-var": "error", // disallow labels that share a name with a variable
+        "no-shadow-restricted-names": "error", // disallow shadowing of names such as arguments
+        "no-undef-init": "error", // disallow use of undefined when initializing variables
+        "no-unused-vars": "error", // disallow declaration of variables that are not used in the code
         "no-use-before-define": ["error", {"functions": false, "classes": true}], // disallow use of variables before they are defined
 
         //
@@ -99,10 +100,10 @@ module.exports = {
         //
         // These rules are only relevant to ES6 environments and are off by default.
         //
-        "no-useless-rename": 2,
-        "no-var": 2, // require let or const instead of var (off by default)
+        "no-useless-rename": "error",
+        "no-var": "error", // require let or const instead of var (off by default)
         "generator-star-spacing": [2, "before"], // enforce the spacing around the * in generator functions (off by default)
-        "no-duplicate-imports": 2,
+        "no-duplicate-imports": "error",
 
         // React
         "react/prop-types": "warn"
